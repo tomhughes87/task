727690a28ee54615f3c1f279b45ca713d82a7cec
Tom Hughes <tomhughes1987@gmail.com>
2022-12-20 15:40:04 +0100

    add the main section and style

--- src/Components/MainContainer/Index.tsx
+++ src/Components/MainContainer/Index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import BadgeCard from "../BadgeCard/index";
 import "./styles.css";
+import "../../App.css";
+import BadgeSection from "../BadgeSection/BadgeSection";
 
 export default function MainContrainer() {
   return (
     <div id="main-contrainer">
-      <div id="header-main-contain">
+      <div id="header-main-container" className="our-border">
         <p>Badges</p>
       </div>
+      <BadgeSection />
+      <div id="content-main-container"></div>
     </div>
   );
 }
